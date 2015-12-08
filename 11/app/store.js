6f2a23cf27c10b7cebab62346ec19b2cf773fba6
tangjing <tangjing@tangjing.com>
2015-12-08 15:43:03 +0800

    vue firebase demo

---
+++ 11/app/store.js
@@ -0,0 +1,67 @@
+// store
+import Firebase from 'firebase';
+import { EventEmitter } from 'events';
+import { Promise } from 'es6-promise'; 
+
+const config = {
+    base: 'https://sweltering-fire-8263.firebaseio.com/demos/',
+    sub: 'todos'
+}
+const api = new Firebase(config['base'] + config['sub']);
+const store = new EventEmitter(); 
+let storeData  = {};
+
+api.on('value', data => {
+  storeData['list'] = data.val();
+  store.emit('data-updated');
+});
+
+store.on('data-updated', e =>{
+    e = e || {};
+    if(e.callback){
+        e.callback();
+    }
+});
+
+export default {
+    add (id, doc){
+        return new Promise((resolve, reject) => {
+            api.once('child_added', node => {
+                let item = node.val();
+                item.id = node.key(); 
+                storeData['list'].push(doc);
+                resolve(storeData['list']);
+            },reject); 
+            api.child(id).set(doc);
+        });
+    },
+    update (id,doc){
+        return new Promise((resolve, reject) => {
+            api.child(id).update(doc, err=>{
+                if(err){
+                    reject(err);
+                }
+                else{
+                    storeData['list'][id] = doc;
+                    resolve(storeData['list']);
+                }
+            });
+        });  
+    },
+    delete (id){
+        return new Promise((resolve, reject) => {
+            api.once('child_removed', node=>{ 
+                delete storeData['list'][id];
+                resolve(storeData['list']);
+            },reject);  
+            new Firebase(config['base'] + config['sub'] + '/' + id).remove();
+        });
+    },
+    getAll (){  
+        return new Promise((resolve, reject) => {
+            api.once('value', data => {
+                resolve(data.val());
+            }, reject);
+        });
+    }
+}
--- README.md
+++ README.md
@@ -3,4 +3,60 @@ vue-demo
 ***
 npm install
 
-wepback
+wepback  
+
+* * *
+
+### demo 1-3
+
+vue router
+
+1.  vue
+2.  vue-router 
+
+* * *
+
+### demo 4-7
+
+vue lazy load img,data 
+
+1.  vue
+2.  component 
+
+* * *
+
+### demo 8
+
+vue swipe 
+
+1.  vue
+2.  component 
+
+* * *
+
+### demo 9
+
+vue iscroll 
+
+1.  vue
+2.  component 
+
+* * *
+
+### demo 10
+
+vue collapsing 
+
+1.  vue
+2.  directive
+3.  es6
+
+* * *
+
+### demo 11
+
+vue+firebase todolist
+
+1.  vue
+2.  firebase
+3.  es6
